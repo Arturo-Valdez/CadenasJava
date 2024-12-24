ae820a9819bbb322cd6052e0214d598f63baf18a
Arturo Valdez <santiguardado27@gmail.com>
2024-12-23 20:15:45 -0600

    Cadenas

--- src/Cadenas.java
+++ src/Cadenas.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
-
 public class Cadenas {
+    public static void main(String[] args) {
+        System.out.println("*** Manejo de Cadenas ***");
+        var cadena1 = "Hola";
+        var cadena2 = new String("Mundo");
+        var cadena3 = cadena1 + " " + cadena2;
+        var cadena4 = """
+                Este es un texto
+                multilinea
+                mas
+                Lineas
+                """;
+
+
+        System.out.println("cadena1 = " + cadena1);
+        System.out.println("cadena2 = " + cadena2);
+        System.out.println("cadena3 = " + cadena3);
+        System.out.println("cadena4 = " + cadena4);
+    }
 }
